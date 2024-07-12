7a1aca609624b50c092e9e042acc5d99d0ff95d8
Norluciot <norluciot.adrien@gmail.com>
2024-07-13 00:20:13 +0300

    Navbar css done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
+        <li>Shop <hr/></li>
         <li>Men</li>
         <li>Women</li>
         <li>Kids</li>
@@ -19,6 +19,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );

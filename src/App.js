6e7bd85998366b5d56806ca80bfb7cac7a5126c6
Norluciot <norluciot.adrien@gmail.com>
2024-07-19 22:09:55 +0300

    Route Creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop/>} />
+          <Route path="/mens" element={<ShopCategory category="men"/>} />
+          <Route path="/womes" element={<ShopCategory category="women"/>} />
+          <Route path="/kids" element={<ShopCategory category="kid"/>} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }

8700f6fb6909063743ad78616d92bf05596c40f3
Norluciot <norluciot.adrien@gmail.com>
2024-07-20 23:10:03 +0300

    Debut Hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left"></div>
+      <div className="hero-left"></div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li> 
+        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none'}}  to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none'}}  to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none'}}  to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li> 
       </ul>
       <div className="nav-login-cart">
-        <Link to='/login'><button>Login</button></Link>
-        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+        <Link style={{textDecoration: 'none'}}  to='/login'><button>Login</button></Link>
+        <Link style={{textDecoration: 'none'}}  to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>

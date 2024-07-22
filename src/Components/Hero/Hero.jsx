19558c30bdcdd799252e2881151087ae35387982
Norluciot <norluciot.adrien@gmail.com>
2024-07-22 22:33:19 +0300

    Hero componenent

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-left"></div>
-      <div className="hero-left"></div>
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hand-hand-hand">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-left-btn">
+          <div className="">Latest Collections</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
     </div>
   );
 };

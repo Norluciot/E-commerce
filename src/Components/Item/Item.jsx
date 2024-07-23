dd1c119943357fc8e642a5c31a5f90c0759ec0da
Norluciot <norluciot.adrien@gmail.com>
2024-07-23 10:08:22 +0300

    Start Offer components

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
-          {props.new_price}
+          ${props.new_price}
         </div>
         <div className="item-price-old">
-          {props.old_price}
+          ${props.old_price}
         </div>
       </div>
     </div>

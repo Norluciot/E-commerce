57762839fdeb8d80f3aeb18a4ddad1ed04c3601c
Norluciot <norluciot.adrien@gmail.com>
2024-07-23 13:54:42 +0300

    Footer Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path='/login' element={<LoginSignUp/>} />
 
         </Routes>
+        <Footer />
 
       </BrowserRouter>
     </div>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -5,6 +5,7 @@ import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
+
 const Shop = () => {
   return (
     <div>
@@ -13,6 +14,7 @@ const Shop = () => {
       <Offers/>
       <NewCollections/>
       <NewsLetter/>
+
     </div>
   )
 }

47631d371b97170f9174ac8ab8ee0a7421cc8226
Norluciot <norluciot.adrien@gmail.com>
2024-07-23 12:37:51 +0300

    NewsLetter Done

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './NewsLetter.css'
+import React from "react";
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
@@ -7,11 +7,13 @@ const NewsLetter = () => {
       <div className="newsletter">
         <h1>Get Exlusive Ofeers On Your Email</h1>
         <p>Subscribe to our newletter and stay updated</p>
+        <div>
+          <input type="email" placeholder="Your Email id" />
+          <button>Subscribe</button>
+        </div>
       </div>
-      <input type="email" placeholder='Your Email id' />
-      <button>Subscribe</button>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;

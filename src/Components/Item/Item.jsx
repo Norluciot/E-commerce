6eb75d014b68428a48e415245cc2fc49f969471f
Norluciot <norluciot.adrien@gmail.com>
2024-07-23 09:14:25 +0300

    Item to shop done

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,6 +5,15 @@ const Item = (props) => {
   return (
     <div className='item'>
       <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          {props.new_price}
+        </div>
+        <div className="item-price-old">
+          {props.old_price}
+        </div>
+      </div>
     </div>
   )
 }

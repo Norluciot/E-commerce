a4712cc7b237ac214771f519b68fc7d6083ab8b2
Norluciot <norluciot.adrien@gmail.com>
2024-07-23 14:45:02 +0300

    Start banner

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop/>} />
-          <Route path='/mens' element={<ShopCategory category="men"/>} />
-          <Route path='/womens' element={<ShopCategory category="women"/>} />
-          <Route path='/kids' element={<ShopCategory category="kid"/>} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>} />
           <Route path='/product' element={<Product/>}>
             <Route path=':productId' element={<Product/>}/>
           </Route>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
 import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
 
-  return <div className="shop-category"></div>;
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+    
+  );
 };
 
 export default ShopCategory;

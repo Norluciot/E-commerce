c82b1dfd4bfd5453a1e962d362bce5c68006ebc3
Norluciot <norluciot.adrien@gmail.com>
2024-07-28 22:47:48 +0300

    STart Css ProductDIsplay

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const porduct = all_product.find((e)=> e.id === Number(productId))
+
   return (
     <div>
-      
+      <Breadcrum product={porduct} />
+      <ProductDisplay product={porduct}  />   
     </div>
   )
 }
